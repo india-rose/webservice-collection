dbfc5b20a9e8336a038c48bd2a31cb79d675becd
Julien Mialon <julien.mialon@backelite.com>
2015-10-12 11:48:40 +0200

    add missing file

---
+++ WebAPI.Common/Responses/VersionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WebAPI.Common.Responses
+{
+	public class VersionResponse
+	{
+		public long Version { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public VersionResponse()
+		{
+			
+		}
+
+		public VersionResponse(long version, DateTime date)
+		{
+			Version = version;
+			Date = date;
+		}
+	}
+}
