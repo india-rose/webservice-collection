0082cad6849c064ca2382113cee448381ad2ee5b
Julien Mialon <julien.mialon@backelite.com>
2015-10-15 10:01:12 +0200

    Reshaper issue

--- WebAPI/Controllers/IndiagramWriteController.cs
+++ WebAPI/Controllers/IndiagramWriteController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
--- WebAPI/Migrations/201510051518404_Initial.cs
+++ WebAPI/Migrations/201510051518404_Initial.cs
@@ -1,7 +1,6 @@
 namespace WebAPI.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
+	using System.Data.Entity.Migrations;
     
     public partial class Initial : DbMigration
     {
